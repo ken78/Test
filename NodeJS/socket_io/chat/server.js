f6062b63827fbc2c34d39366fa3ecd4b3faca0bc
Sayasith Pon <pon.sayasith@igt.com>
2015-05-22 08:51:15 -0700

    format commented code to end of file to be readable

--- NodeJS/socket_io/chat/server.js
+++ NodeJS/socket_io/chat/server.js
@@ -4,27 +4,10 @@ var app = express();
 app.set('port', 8080);
 app.use(express.static(__dirname));
 
-// app.configure(
-//     function() {
-//
-//       app.set('port', 8080);
-//       app.use(express.static(__dirname));
-//     });
-
-// var http = require('http').Server(app);
-
 var http = require('http');
 var server = http.createServer(app).listen(app.get('port'));
 
-// var io = require('socket.io');
-// io.listen(http);
-
 var io = require('socket.io').listen(server);
-
-// var io = require('socket.io')(http);
-
-// var socketIo = require('socket.io')(http);
-// var io = socketIo(http);
 
 // ===============================================================================
 // Setup routes handler for url that does http "Get"
@@ -50,6 +33,27 @@ io.on('connection',
     });
 
 // ===============================================================================
+
+
+
+
+// var http = require('http').Server(app);
+
+// app.configure(
+//     function() {
+//
+//       app.set('port', 8080);
+//       app.use(express.static(__dirname));
+//     });
+
+// var io = require('socket.io');
+// io.listen(http);
+
+// var io = require('socket.io')(http);
+
+// var socketIo = require('socket.io')(http);
+// var io = socketIo(http);
+
 
 // If the below throw error on linux workstation box.
 // http.listen(process.env.PORT || 8080);
